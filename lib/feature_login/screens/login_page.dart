a00cda964c9b71fbbd23f013dd6d2c89ba92dda4
Ashutosh4192 <123867114+Ashutosh4192@users.noreply.github.com>
2024-10-18 23:15:27 +0530

    featue_login added

--- lib/feature_login/screens/login_page.dart
+++ lib/feature_login/screens/login_page.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:urja_app/feature_login/widgets/my_button.dart';
+import 'package:urja_app/feature_login/widgets/my_textfield.dart';
+import 'package:urja_app/bottomnavigation.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    void signUserIn() {
+      (usernameController.text == "246510057" &&
+              passwordController.text == "Ashu@123")
+          ? Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const Bottomnav()))
+          : Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+
+            // logo
+            const Icon(
+              Icons.lock,
+              size: 100,
+              color: Colors.green,
+            ),
+
+            const SizedBox(height: 50),
+
+            // welcome back, you've been missed!
+            const Text(
+              'Welcome back you\'ve been missed!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // username textfield
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Consumer ID',
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            // password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            // // forgot password?
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.end,
+            //     children: [
+            //       Text(
+            //         'Forgot Password?',
+            //         style: TextStyle(color: Colors.red),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+            const SizedBox(height: 25),
+
+            // sign in button
+            MyButton(
+              onTap: signUserIn,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/feature_splash/screens/spalsh_screen.dart
+++ lib/feature_splash/screens/spalsh_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:urja_app/bottomnavigation.dart';
+import 'package:urja_app/feature_login/screens/login_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const Bottomnav()));
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
     });
   }
 
